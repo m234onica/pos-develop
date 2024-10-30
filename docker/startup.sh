b13bb499c77ecbce62f97c1e6d0b695298762017
joycehsu <joycehsu@25sprout.com>
2024-10-31 02:48:18 +0800

    fix: 改本地端

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,7 +4,7 @@ sed -i "s,LISTEN_PORT,${PORT:-80},g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
-while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+while ! nc -w 1 -z 0.0.0.0 9000; do sleep 0.1; done;
 
 if ! nginx; then
   echo "Nginx failed to start."
